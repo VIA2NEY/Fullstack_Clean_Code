b2a40ed8d5e892651c51ef92dfabf7e89cec4227
VIA2NEY <kouameangeV11@gmail.com>
2025-03-04 17:20:13 +0000

    Ajout des routes pour modif et suppression

--- Server/routes/routes_products.py
+++ Server/routes/routes_products.py
@@ -36,3 +36,34 @@ def get_product(product_id: int, db: Session = Depends(get_db)):
         raise_exception(404, "Produit non trouvé", f"le produit avec l'id {product_id} n'existe pas")
 
     return ProductDetailResponse(code=200, message="Produit récupéré", data=product)
+
+
+@router_product.put("/{product_id}", response_model=ProductDetailResponse)
+def update_product(product_id: int, product: ProductCreate, db: Session = Depends(get_db)):
+    db_product = db.query(Product).filter(Product.id == product_id).first()
+
+    if not db_product:
+        raise_exception(404, "Produit non trouvé", f"Le produit avec l'ID {product_id} n'existe pas")
+
+    # Mise à jour des champs
+    db_product.name = product.name
+    db_product.description = product.description
+    db_product.price = product.price
+
+    db.commit()
+    db.refresh(db_product)
+
+    return ProductDetailResponse(code=200, message="Produit mis à jour avec succès", data=db_product)
+
+
+@router_product.delete("/{product_id}", response_model=ProductDetailResponse)
+def delete_product(product_id: int, db: Session = Depends(get_db)):
+    db_product = db.query(Product).filter(Product.id == product_id).first()
+
+    if not db_product:
+        raise_exception(404, "Produit non trouvé", f"Le produit avec l'ID {product_id} n'existe pas")
+
+    db.delete(db_product)
+    db.commit()
+
+    return ProductDetailResponse(code=200, message="Produit supprimé avec succès", data= None)
